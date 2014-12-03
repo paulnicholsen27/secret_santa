d66109a95ef76ac57e34c4ac194079345f57cd48
Paul Nichols <pnichols@theatlantic.com>
2014-12-03 10:49:10 -0500

    added html formatting, testing flag

--- secret_santa.py
+++ secret_santa.py
@@ -1,7 +1,11 @@
 import random
 import pprint
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from config import PASSWORD
+
+TESTING = True
 
 players = [{'name': 'Paul', 'email': 'pnichols104@gmail.com', 'elf': '', 'exclude':['Jason']},
 		  {'name': 'Jason', 'email': 'jason.b.olsen@gmail.com', 'elf': '', 'exclude':['Paul']},
@@ -23,7 +27,6 @@ def assign_elves():
 				if players[i]['name'] != santas[i] and santas[i] not in players[i]['exclude']:
 					players[i]['elf'] = santas[i]
 				else:
-
 					potential = False
 					break
 			if potential:
@@ -34,14 +37,59 @@ assign_elves()
 
 SERVER = 'localhost'
 for player in players:
-	print "%s is the Secret Santa for %s!" %(player['name'], player['elf'])
-	msg = MIMEText("Hello %s!  Merry Christmas!  Your secret elf is %s!" %(player['name'], player['elf']))
+	
+	HMTL = """
+	<html>
+	<head></head>
+	<body>
+	<p>
+	Hello <strong>%s</strong>!  Merry Christmas!<br><br>
 
-msg['From'] = 'pnichols104@gmail.com'
-msg['To'] = 'pnichols104@gmail.com'
-msg['Subject'] = 'Secret Santa'
+	This year we've decided to play Secret Santa and Yankee Swap.  
+	For Secret Santa, please buy a gift for your secret elf, <strong>%s</strong>!  
+	There is a price limit of $50 for Secret Santa gifts.<br><br>
 
-s = smtplib.SMTP(SERVER, 1025)
-s.sendmail('pnichols104@gmail.com', ['pnichols104@gmail.com'], msg.as_string())
-# import pdb; pdb.set_trace()
-s.quit()
+	For Yankee Swap, please purchase a gift for a grab-bag costing no more than $25.<br><br>
+
+	Note:  This was sent anonymously so I won't know who has who.  
+	PLEASE DO NOT REPLY DIRECTLY TO THIS MESSAGE or you will ruin my Christmas.<br><br>
+
+	Love,<br>
+	Paul
+	</p>
+	</body>
+	</html>
+	""" %(player['name'], player['elf'])
+
+	TEXT = """
+	Hello %s!  Merry Christmas!
+
+	This year we've decided to play Secret Santa and Yankee Swap.  
+	For Secret Santa, please buy a gift for your secret elf, %s!  
+	There is a price limit of $50 for Secret Santa gifts.
+
+	For Yankee Swap, please purchase a gift for a grab-bag costing no more than $25.
+
+	Note:  This was sent anonymously so I won't know who has who.  
+	PLEASE DO NOT REPLY DIRECTLY TO THIS MESSAGE or you will ruin my Christmas.
+
+	Love,
+	Paul
+	""" %(player['name'], player['elf'])
+
+	FROM = 'pnichols104@gmail.com'
+	TO = 'pnichols104@gmail.com' if TESTING else [player['email']]
+	SUBJECT = 'TEST SECRET SANTA' if TESTING else 'Secret Santa'
+
+	msg = MIMEMultipart('alternative')
+	part1 = MIMEText(TEXT, 'plain')
+	part2 = MIMEText(HMTL, 'html')
+	msg.attach(part1)
+	msg.attach(part2)
+	server = smtplib.SMTP('smtp.gmail.com',587) #port 465 or 587
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+	server.login('pnichols104@gmail.com', PASSWORD)
+	server.sendmail(FROM, TO, msg.as_string())
+	server.close()
